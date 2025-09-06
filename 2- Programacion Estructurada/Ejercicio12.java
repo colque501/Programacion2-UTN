f3d790de39bf97c6f3a373dbf82e02599b2889ee
colque501 <briangutierrezcolque@hotmail.com>
2025-09-06 10:52:09 -0300

    Create Ejercicio12.java

---
+++ 2- Programacion Estructurada/Ejercicio12.java
@@ -0,0 +1,19 @@
+/**
+*
+* @author Brian
+* 
+*/
+public class ejercicio12 {
+    public static void main(String[] args) {
+        double [] precios = {199.99, 299.5, 149.75, 399.0, 89.99};
+        System.out.println("Precios originales:");
+        for (int i = 1; i<=precios.length; i++){
+            System.out.println("Precio: $"+ precios[i-1]);
+        }
+        precios[2] = 129.99;
+        System.out.println("Precios modificados:");
+        for (int i = 1; i<=precios.length; i++){
+            System.out.println("Precio: $"+ precios[i-1]);
+        }
+    }
+}
