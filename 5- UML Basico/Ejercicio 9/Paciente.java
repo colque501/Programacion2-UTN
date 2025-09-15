1c66373c4cd93b418ec0195ecd9e50090c93b9cb
colque501 <briangutierrezcolque@hotmail.com>
2025-09-15 18:58:56 -0300

    Create Paciente.java

---
+++ 5- UML Basico/Ejercicio 9/Paciente.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ej9;
+
+/**
+*
+* @author Brian
+* 
+*/
+public class Paciente {
+    private String nombre;
+    private String obraSocial;
+
+    public Paciente(String nombre, String obraSocial) {
+        this.nombre = nombre;
+        this.obraSocial = obraSocial;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getObraSocial() {
+        return obraSocial;
+    }
+
+    @Override
+    public String toString() {
+        return "Paciente{" +
+               "nombre=" + nombre +
+               ", obraSocial=" + obraSocial +
+               '}';
+    }
+}
