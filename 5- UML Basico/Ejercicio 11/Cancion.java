4ec86b7cd68b8f31916e0a92c8963d8fc3351444
colque501 <briangutierrezcolque@hotmail.com>
2025-09-16 18:44:33 -0300

    Create Cancion.java

---
+++ 5- UML Basico/Ejercicio 11/Cancion.java
@@ -0,0 +1,37 @@
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ej11;
+
+/**
+*
+* @author Brian
+* 
+*/
+public class Cancion {
+    private String titulo;
+    private Artista artista;
+
+    public Cancion(String titulo, Artista artista) {
+        this.titulo = titulo;
+        this.artista = artista;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public Artista getArtista() {
+        return artista;
+    }
+
+    @Override
+    public String toString() {
+        return "Cancion{" +
+               "titulo=" + titulo +
+               ", artista=" + (artista != null ? artista.getNombre() : "null") +
+               '}';
+    }
+}
