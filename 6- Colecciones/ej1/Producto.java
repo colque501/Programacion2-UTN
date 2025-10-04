c8973ee25ca72cef972485dc7ba51c7c77c9d0db
colque501 <briangutierrezcolque@hotmail.com>
2025-10-04 08:38:52 -0300

    Create Producto.java

---
+++ 6- Colecciones/ej1/Producto.java
@@ -0,0 +1,46 @@
+package ej1;
+
+public class Producto {
+    private String id;
+    private String nombre;
+    private double precio;
+    private int cantidad;
+    private CategoriaProducto categoria;
+
+    public Producto(String id, String nombre, double precio, int cantidad, CategoriaProducto categoria) {
+        this.id = id;
+        this.nombre = nombre;
+        this.precio = precio;
+        this.cantidad = cantidad;
+        this.categoria = categoria;
+    }
+
+    public Producto(String id, String nombre, double precio, CategoriaProducto categoria) {
+        this(id, nombre, precio, 0, categoria);
+    }
+
+    public void mostrarInfo() {
+        System.out.println(this);
+    }
+
+    @Override
+    public String toString() {
+        return "Producto{" +
+                "id='" + id + '\'' +
+                ", nombre='" + nombre + '\'' +
+                ", precio=" + precio +
+                ", cantidad=" + cantidad +
+                ", categoria=" + categoria + " (" + categoria.getDescripcion() + ")" +
+                '}';
+    }
+
+    public String getId() { return id; }
+    public String getNombre() { return nombre; }
+    public double getPrecio() { return precio; }
+    public int getCantidad() { return cantidad; }
+    public CategoriaProducto getCategoria() { return categoria; }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+}
