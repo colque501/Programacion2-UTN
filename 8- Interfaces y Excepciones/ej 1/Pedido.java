077b69b48e01bc81c16e09bbde49bff87321582c
colque501 <briangutierrezcolque@hotmail.com>
2025-11-17 19:19:16 -0300

    Create Pedido.java

---
+++ 8- Interfaces y Excepciones/ej 1/Pedido.java
@@ -0,0 +1,39 @@
+package ej1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Pedido implements Pagable {
+    private List<Producto> productos;
+    private String estado;
+    private Cliente cliente;
+
+    public Pedido(Cliente cliente) {
+        this.cliente = cliente;
+        this.productos = new ArrayList<>();
+        this.estado = "Pendiente";
+    }
+
+    public void agregarProducto(Producto p) {
+        productos.add(p);
+    }
+
+    public void cambiarEstado(String nuevoEstado) {
+        this.estado = nuevoEstado;
+        cliente.notificarCambio("El pedido cambió su estado a: " + nuevoEstado);
+    }
+
+    @Override
+    public double calcularTotal() {
+        double total = 0;
+        for (Producto p : productos) {
+            total += p.calcularTotal();
+        }
+        return total;
+    }
+
+    @Override
+    public String toString() {
+        return "Pedido (" + estado + ") - Total: $" + calcularTotal();
+    }
+}
