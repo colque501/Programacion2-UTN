acc16bafc084cfccabbe48d2f8b79e48bde841eb
colque501 <briangutierrezcolque@hotmail.com>
2025-11-17 19:17:54 -0300

    Create PagoPayPal.java

---
+++ 8- Interfaces y Excepciones/ej 1/PagoPayPal.java
@@ -0,0 +1,8 @@
+package ej1;
+
+public class PagoPayPal implements Pago {
+    @Override
+    public void procesarPago(double monto) {
+        System.out.println("Pago con PayPal procesado por $" + monto);
+    }
+}
